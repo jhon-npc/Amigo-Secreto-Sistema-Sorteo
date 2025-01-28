02b9e5c254b9a71dec94ffd853cbef8bc5e6d5f6
jhon-npc <jh.nicolaspacheco@gmail.com>
2025-01-28 18:10:20 -0500

    Sortea de amigo secreto completado

--- app.js
+++ app.js
@@ -1,12 +1,62 @@
 let listaAmigos = [];
+ocultarResultado();
 
 function insertarTexto(id,texto){
     let elementoHTML = document.getElementById(id);
     elementoHTML.innerHTML = texto;
 }
 
+function insertarLista(id,texto){
+    let elementoHTML = document.getElementById(id);
+    let nuevoElemento = document.createElement('li');
+    nuevoElemento.textContent = texto;
+    elementoHTML.appendChild(nuevoElemento); 
+}
+
 function agregarAmigo(){
     let amigoIngresado = document.getElementById('amigo').value;
-    listaAmigos.push(amigoIngresado);
-    insertarTexto('listaAmigos',listaAmigos);
+    if(amigoIngresado == '' || !isNaN(amigoIngresado)){
+        alert("Por favor, inserte un nombre");
+    }else if(listaAmigos.includes(amigoIngresado)){
+        insertarTexto('nuevoAmigo','Por favor, ingrese un nuevo nombre');
+    }else{
+        listaAmigos.push(amigoIngresado);
+        insertarLista('listaAmigos',amigoIngresado);
+        limpiarTextos();
+        mostrarLista();
+        ocultarResultado();
+    }
+}
+
+function limpiarTextos(){
+    document.getElementById('amigo').value = '';
+    insertarTexto('nuevoAmigo','');
+}
+
+function sortearAmigo(){
+    if(listaAmigos.length == 1){
+        insertarTexto('nuevoAmigo','Por favor, ingrese un nombre adicional para realizar el sorteo');
+    }else{
+        ocultarLista();
+        let numeroSorteado = Math.floor(Math.random()*listaAmigos.length);
+        let amigoGanador = listaAmigos[numeroSorteado];
+        insertarTexto('resultado',`El amigo secreto sorteado es: ${amigoGanador}`);
+        mostrarResultado();
+    }
+}
+
+function mostrarLista() {
+    document.getElementById('listaAmigos').style.display = "block";
+}
+
+function ocultarLista() {
+    document.getElementById('listaAmigos').style.display = "none";
+}
+
+function mostrarResultado() {
+    document.getElementById('resultado').style.display = "block";
+}
+
+function ocultarResultado() {
+    document.getElementById('resultado').style.display = "none";
 }
